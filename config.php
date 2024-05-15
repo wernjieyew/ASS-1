ed8fe280d948670e7930599e486b5436a6902a36
wernjieyew <110628646+wernjieyew@users.noreply.github.com>
2024-05-15 17:09:55 +0800

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,10 @@ ini_set('display_errors', '1'); // Turn it off for production
 error_reporting(E_ALL);
 
 $currency = '₹';
-$db_username = 'root';
-$db_password = '';
+$db_username = 'admin';
+$db_password = 'database';
 $db_name = 'bolt';
-$db_host = 'localhost';
+$db_host = 'assdatabase.cuxnkfqlkpwa.us-east-1.rds.amazonaws.com';
 
 $mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
 
